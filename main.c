812ab1c13097ca21eacd029910de71930f62018e
Replit user <>
2023-08-29 16:00:49 +0000

    love it

--- main.c
+++ main.c
@@ -1,26 +1,23 @@
+#include "solve.h"
 #include <stdio.h>
 #include <string.h>
-#include "solve.h"
-
-
 
 int main() {
-  // value to substitute for X  
+  // value to substitute for X
   int x_value = 3;
 
   char *thing = "x + 3";
   int length = strlen(thing);
-  
-  int numbers[2] = {3,3};
-  char operations[1] = {"+"};
 
+  int numbers[2] = {3, 3};
+  char operations[1] = {"+"};
 
   // Solve Addition
   int index;
-  int solved_value;
+  int solved_value = numbers[0];
 
-  for (index = 0; index < (sizeof(operations) / sizeof(operations[0])); index++) {
-    
+  for (index = 0; index < (sizeof(operations) / sizeof(operations[0]));
+       index++) {
   }
 
   return 0;
