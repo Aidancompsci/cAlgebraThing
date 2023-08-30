7781d5322967f1ea41bd1af02155b699e8b4b833
Replit user <>
2023-08-30 16:34:04 +0000

    af

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 
+
+
+
 int main() {
   // value to substitute for X
   int x_value = 3;
@@ -15,21 +18,37 @@ int main() {
   // Solve Multiplicaiton & Division
   int mult_index;
   char new_operations[sizeof(operations) / sizeof(operations[0])];
-  char new_numbers[sizeof(numbers) / sizeof(numbers[0])];
+  int new_numbers[sizeof(numbers) / sizeof(numbers[0])];
 
-  
-  for (mult_index = 0; mult_index < (sizeof(operations) / sizeof(operations[0])); mult_index++) {
+  int new_index = 0;
+
+  for (mult_index = 0;
+       mult_index < (sizeof(operations) / sizeof(operations[0]));
+       mult_index++) {
+    // Currently Selected Operation
     char current_operation = operations[mult_index];
+    // Used numbers
+    int current_number = numbers[mult_index];
+    int next_number = numbers[mult_index];
+         
+    if (current_operation == '*') {
+      new_numbers[new_index] *= next_number;
+    } else {
+      new_operations[new_index] = current_operation;
+      new_numbers[new_index] = current_number;
+      new_index++;
+    }
   }
 
+  
   // Solve Addition & Subtraction
   int add_index;
-  int solved_value = numbers[0];
+  int solved_value = new_numbers[0];
 
-  for (add_index = 0; add_index < (sizeof(operations) / sizeof(operations[0]));
+  for (add_index = 0; add_index < (sizeof(new_operations) / sizeof(new_operations[0]));
        add_index++) {
-    char current_operation = operations[add_index];
-    solved_value = solve_operation(solved_value, numbers[add_index + 1],
+    char current_operation = new_operations[add_index];
+    solved_value = solve_operation(solved_value, new_numbers[add_index + 1],
                                    current_operation);
   }
 
